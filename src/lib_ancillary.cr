71cd046ee35be21c655bcae48afde2c1411d2329
Carl Hörberg <carl.hoerberg@gmail.com>
2020-03-30 15:08:09 +0200

    multiple FDs binding

--- src/lib_ancillary.cr
+++ src/lib_ancillary.cr
@@ -1,5 +1,9 @@
 @[Link(ldflags: "#{__DIR__}/../libancillary/libancillary.a", static: true)]
 lib LibAncillary
+  MAX_FDS = 960
+
   fun ancil_send_fd(socket : LibC::Int, file_descriptor : LibC::Int) : LibC::Int
+  fun ancil_send_fds(socket : LibC::Int, fds : LibC::Int*, n_fds : LibC::UInt) : LibC::Int
   fun ancil_recv_fd(socket : LibC::Int, file_descriptor : LibC::Int*) : LibC::Int
+  fun ancil_recv_fds(socket : LibC::Int, fds : LibC::Int*, n_fds : LibC::UInt) : LibC::Int
 end
