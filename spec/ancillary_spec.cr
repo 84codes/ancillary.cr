8d8881c3ecfd8df1ed9a630e84f8dd728577e503
Carl Hörberg <carl.hoerberg@gmail.com>
2020-03-30 19:48:25 +0200

    get rid of fork in spec

--- spec/ancillary_spec.cr
+++ spec/ancillary_spec.cr
@@ -2,25 +2,39 @@ require "./spec_helper"
 
 describe Ancillary do
   it "can send and receive a file descriptor" do
-    File.delete "/tmp/ancilspec" if File.exists? "/tmp/ancilspec"
-    fork do
-      File.open("/tmp/ancilspecfile", "w") do |f|
+    file_path = File.tempname "spec", ".txt"
+    sock_path = File.tempname "spec", ".sock"
+    begin
+      code = <<-CODE
+      #!/usr/bin/env crystal
+      require "./src/ancillary"
+      File.open("#{file_path}", "w") do |f|
         f.sync = true
         f.print "foo"
-        UNIXServer.open("/tmp/ancilspec") do |s|
+        UNIXServer.open("#{sock_path}") do |s|
           client = s.accept
-          ancil = Ancillary.new(client)
-          ancil.send(f.fd)
+          client.blocking = true
+          Ancillary.send(client, f.fd)
         end
       end
+      CODE
+      File.write("spec-server", code, 0x755)
+      server_process = Process.new "./spec-server"
+      until File.exists? sock_path
+        sleep 0.1
+        server_process.terminated?.should be_false
+      end
+      c = UNIXSocket.new(sock_path)
+      ancil = Ancillary.new(c)
+      fd = ancil.receive
+      io = IO::FileDescriptor.new(fd)
+      io.print "bar"
+      io.close
+      File.read(file_path).should eq "foobar"
+    ensure
+      File.delete(file_path) if File.exists? file_path
+      File.delete(sock_path) if File.exists? sock_path
+      File.delete("spec-server") if File.exists? "spec-server"
     end
-    sleep 0.1
-    c = UNIXSocket.new("/tmp/ancilspec")
-    ancil = Ancillary.new(c)
-    fd = ancil.receive
-    io = IO::FileDescriptor.new(fd)
-    io.print "bar"
-    io.close
-    File.read("/tmp/ancilspecfile").should eq "foobar"
   end
 end
